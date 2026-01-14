a40ecf77bc7e4bff547cecc1a9dfe5a8000f753b
Madison Sadler <madison.sadler87@gmail.com>
2026-01-13 20:34:03 -0600

    Fix header import grouping

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,9 +2,7 @@ import { useState } from 'react';
 import classNames from 'classnames/bind';
 import { FaBars, FaCode, FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
-
 import appConfig from 'app.config';
-
 import { NavigationMenu, SkipNavigationLink } from '../';
 
 import styles from './Header.module.scss';
